d0c447c5fc521fb834f72bda9de2ab816a0b1e26
supermaniszhou <741963634@qq.com>
2021-02-07 09:41:29 +0800

    update

--- pulldata/src_meeting/com/seeyon/cap4/form/modules/engin/base/formData/fw/FwHttpSend.java
+++ pulldata/src_meeting/com/seeyon/cap4/form/modules/engin/base/formData/fw/FwHttpSend.java
@@ -78,120 +78,143 @@ public class FwHttpSend implements Runnable {
         this.map = map;
     }
 
+    /**
+     * 会议通知是发送到股份公司的
+     * @param pMap
+     * @param id
+     * @param names
+     */
     public void toSendOfFw(Map<String, Object> pMap, Long id, List<String> names) {
         ProptiesUtil prop = new ProptiesUtil();
-        List<Map<String, Object>> mapList = new ArrayList<>();
-        Map<String, Object> map = null;
-        //标题
-        map = new HashMap<>();
-        map.put("fieldName", "tzbt");
-        map.put("fieldValue", (pMap.get("field0002").toString()).replaceAll("\n", "").replaceAll("\r", ""));
-        mapList.add(map);
-        //召开时间
-        map = new HashMap<>();
-        map.put("fieldName", "zksj");
-        map.put("fieldValue", pMap.get("field0003") + "");
-        mapList.add(map);
-        //会议地点
-        map = new HashMap<>();
-        map.put("fieldName", "hydd");
-        map.put("fieldValue", pMap.get("field0001") + "");
-        mapList.add(map);
-        //主文件
-        String mainSql = "select a.file_url,a.filename from " + prop.getOaMeetingFormmain() + " f,CTP_ATTACHMENT a where f.id=" + id + " and a.REFERENCE=f.id and a.SUB_REFERENCE=f.FIELD0006";
-        List<Map<String, Object>> mainFiles = getFile(prop, mainSql);
-        map = new HashMap<>();
-        map.put("fieldName", "zwj");
-        map.put("fieldValue", mainFiles);
-        mapList.add(map);
-        //文号
-        map = new HashMap<>();
-        map.put("fieldName", "wh");
-        map.put("fieldValue", pMap.get("field0007") + "");
-        mapList.add(map);
-        //其他附件
-        String qtFileSql = "select a.file_url,a.filename from " + prop.getOaMeetingFormmain() + " f,CTP_ATTACHMENT a where f.id=" + id + " and a.REFERENCE=f.id and a.SUB_REFERENCE=f.FIELD0012";
-        List<Map<String, Object>> qtFileList = getFile(prop, qtFileSql);
-        map = new HashMap<>();
-        map.put("fieldName", "qtwj");
-        map.put("fieldValue", qtFileList);
-        mapList.add(map);
-        //发布单位
-        map = new HashMap<>();
-        map.put("fieldName", "fbdw");
-        map.put("fieldValue", pMap.get("field0054") + "");
-        mapList.add(map);
-        //发布部门
-        String fbrSql = "select m.name ,(select name from ORG_UNIT u where id=m.ORG_DEPARTMENT_ID) deptName from  ORG_MEMBER m where m.id=" + pMap.get("field0010");
-        List<Map<String, Object>> result = JDBCUtil.doQuery(fbrSql);
-        map = new HashMap<>();
-        map.put("fieldName", "fbbm");
-        map.put("fieldValue", result.get(0).get("deptname") + "");
-        mapList.add(map);
-        //发布人
-        map = new HashMap<>();
-        map.put("fieldName", "fbr");
-        map.put("fieldValue", result.get(0).get("name") + "");
-        mapList.add(map);
-        //发布人手机号码
-        map = new HashMap<>();
-        map.put("fieldName", "fbrsjhm");
-        map.put("fieldValue", pMap.get("field0011") + "");
-        mapList.add(map);
-        //发布人固定电话
-        map = new HashMap<>();
-        map.put("fieldName", "fbrgddh");
-        map.put("fieldValue", pMap.get("field0026") + "");
-        mapList.add(map);
-        //会议联系人
-        List<Map<String, Object>> resultLxr = null;
-        if (null != pMap.get("field0059")) {
-            String lxrSql = "select m.name ,(select name from ORG_UNIT u where id=m.ORG_DEPARTMENT_ID) deptName from  ORG_MEMBER m where m.id=" + pMap.get("field0059");
-            resultLxr = JDBCUtil.doQuery(lxrSql);
+
+        String getUnitIdSql = "select id from ORG_UNIT where path= " + prop.getGfPath();
+        List<Map<String, Object>> unitList = JDBCUtil.doQuery(getUnitIdSql);
+        Map<String, Object> idMap = unitList.get(0);
+        String unitId = idMap.get("id") + "";
+        String getFwUserIdSql = "SELECT nvl(ext_attr_2,'0') fwuserid from ADDRESSBOOK where member_id in (select  SOURCE_ID  from ORG_RELATIONSHIP where OBJECTIVE0_ID=" + unitId + " and OBJECTIVE1_ID=(select id from org_role where ORG_ACCOUNT_ID=" + unitId + " and IS_ENABLE=1 and code ='UnitJyy'))";
+        List<Map<String, Object>> fwUserIds = JDBCUtil.doQuery(getFwUserIdSql);
+        Map<String, Object> fwMap = fwUserIds.get(0);
+        //获取维护的泛微的人员编号
+        String sendUserId = "";
+        String fwuserid = fwMap.get("fwuserid") + "";
+        if (!"0".equals(fwuserid)) {
+            sendUserId = fwuserid;
         }
-        map = new HashMap<>();
-        map.put("fieldName", "hylxr");
-        if (resultLxr.size() > 0) {
-            map.put("fieldValue", resultLxr.get(0).get("name") + "");
-        } else {
-            map.put("fieldValue", "");
-        }
-        mapList.add(map);
-        //会议联系人手机号码
-        map = new HashMap<>();
-        map.put("fieldName", "hylxrsjhm");
-        map.put("fieldValue", null != pMap.get("field0060") ? pMap.get("field0060") + "" : "");
-        mapList.add(map);
-        //会议联系人固定电话
-        map = new HashMap<>();
-        map.put("fieldName", "hylxrgddh");
-        map.put("fieldValue", null != pMap.get("field0061") ? pMap.get("field0061") + "" : "");
-        mapList.add(map);
-        //发布时间
-        map = new HashMap<>();
-        map.put("fieldName", "fbsj");
-        map.put("fieldValue", pMap.get("field0013") + "");
-        mapList.add(map);
-        //截止报送时间
-        map = new HashMap<>();
-        map.put("fieldName", "jzbssj");
-        map.put("fieldValue", pMap.get("field0022") + "");
-        mapList.add(map);
-        //通知对象
-        StringBuilder sb = new StringBuilder();
-        names.forEach(s -> {
-            sb.append(s + "；");
-        });
-        map = new HashMap<>();
-        map.put("fieldName", "tzdx");
-        map.put("fieldValue", sb.toString());
-        mapList.add(map);
-        //会议编号
-        map = new HashMap<>();
-        map.put("fieldName", "hybh");
-        map.put("fieldValue", pMap.get("field0028") + "");
-        mapList.add(map);
-        //开始日期
+
+        if (!"".equals(sendUserId)) {
+            if (names.size() > 0) {
+                List<Map<String, Object>> mapList = new ArrayList<>();
+                Map<String, Object> map = null;
+                //标题
+                map = new HashMap<>();
+                map.put("fieldName", "tzbt");
+                map.put("fieldValue", (pMap.get("field0002").toString()).replaceAll("\n", "").replaceAll("\r", ""));
+                mapList.add(map);
+                //召开时间
+                map = new HashMap<>();
+                map.put("fieldName", "zksj");
+                map.put("fieldValue", pMap.get("field0003") + "");
+                mapList.add(map);
+                //会议地点
+                map = new HashMap<>();
+                map.put("fieldName", "hydd");
+                map.put("fieldValue", pMap.get("field0001") + "");
+                mapList.add(map);
+                //主文件
+                String mainSql = "select a.file_url,a.filename from " + prop.getOaMeetingFormmain() + " f,CTP_ATTACHMENT a where f.id=" + id + " and a.REFERENCE=f.id and a.SUB_REFERENCE=f.FIELD0006";
+                List<Map<String, Object>> mainFiles = getFile(prop, mainSql);
+                map = new HashMap<>();
+                map.put("fieldName", "zwj");
+                map.put("fieldValue", mainFiles);
+                mapList.add(map);
+                //文号
+                map = new HashMap<>();
+                map.put("fieldName", "wh");
+                map.put("fieldValue", pMap.get("field0007") + "");
+                mapList.add(map);
+                //其他附件
+                String qtFileSql = "select a.file_url,a.filename from " + prop.getOaMeetingFormmain() + " f,CTP_ATTACHMENT a where f.id=" + id + " and a.REFERENCE=f.id and a.SUB_REFERENCE=f.FIELD0012";
+                List<Map<String, Object>> qtFileList = getFile(prop, qtFileSql);
+                map = new HashMap<>();
+                map.put("fieldName", "qtwj");
+                map.put("fieldValue", qtFileList);
+                mapList.add(map);
+                //发布单位
+                map = new HashMap<>();
+                map.put("fieldName", "fbdw");
+                map.put("fieldValue", pMap.get("field0054") + "");
+                mapList.add(map);
+                //发布部门
+                String fbrSql = "select m.name ,(select name from ORG_UNIT u where id=m.ORG_DEPARTMENT_ID) deptName from  ORG_MEMBER m where m.id=" + pMap.get("field0010");
+                List<Map<String, Object>> result = JDBCUtil.doQuery(fbrSql);
+                map = new HashMap<>();
+                map.put("fieldName", "fbbm");
+                map.put("fieldValue", result.get(0).get("deptname") + "");
+                mapList.add(map);
+                //发布人
+                map = new HashMap<>();
+                map.put("fieldName", "fbr");
+                map.put("fieldValue", result.get(0).get("name") + "");
+                mapList.add(map);
+                //发布人手机号码
+                map = new HashMap<>();
+                map.put("fieldName", "fbrsjhm");
+                map.put("fieldValue", pMap.get("field0011") + "");
+                mapList.add(map);
+                //发布人固定电话
+                map = new HashMap<>();
+                map.put("fieldName", "fbrgddh");
+                map.put("fieldValue", pMap.get("field0026") + "");
+                mapList.add(map);
+                //会议联系人
+                List<Map<String, Object>> resultLxr = null;
+                if (null != pMap.get("field0059")) {
+                    String lxrSql = "select m.name ,(select name from ORG_UNIT u where id=m.ORG_DEPARTMENT_ID) deptName from  ORG_MEMBER m where m.id=" + pMap.get("field0059");
+                    resultLxr = JDBCUtil.doQuery(lxrSql);
+                }
+                map = new HashMap<>();
+                map.put("fieldName", "hylxr");
+                if (resultLxr.size() > 0) {
+                    map.put("fieldValue", resultLxr.get(0).get("name") + "");
+                } else {
+                    map.put("fieldValue", "");
+                }
+                mapList.add(map);
+                //会议联系人手机号码
+                map = new HashMap<>();
+                map.put("fieldName", "hylxrsjhm");
+                map.put("fieldValue", null != pMap.get("field0060") ? pMap.get("field0060") + "" : "");
+                mapList.add(map);
+                //会议联系人固定电话
+                map = new HashMap<>();
+                map.put("fieldName", "hylxrgddh");
+                map.put("fieldValue", null != pMap.get("field0061") ? pMap.get("field0061") + "" : "");
+                mapList.add(map);
+                //发布时间
+                map = new HashMap<>();
+                map.put("fieldName", "fbsj");
+                map.put("fieldValue", pMap.get("field0013") + "");
+                mapList.add(map);
+                //截止报送时间
+                map = new HashMap<>();
+                map.put("fieldName", "jzbssj");
+                map.put("fieldValue", pMap.get("field0022") + "");
+                mapList.add(map);
+                //通知对象
+                StringBuilder sb = new StringBuilder();
+                names.forEach(s -> {
+                    sb.append(s + "；");
+                });
+                map = new HashMap<>();
+                map.put("fieldName", "tzdx");
+                map.put("fieldValue", sb.toString());
+                mapList.add(map);
+                //会议编号
+                map = new HashMap<>();
+                map.put("fieldName", "hybh");
+                map.put("fieldValue", pMap.get("field0028") + "");
+                mapList.add(map);
+                //开始日期
 //        map = new HashMap<>();
 //        map.put("fieldName", "rq");
 //        map.put("fieldValue", );
@@ -211,47 +234,52 @@ public class FwHttpSend implements Runnable {
 //        map.put("fieldName", "jssj");
 //        map.put("fieldValue", );
 //        mapList.add(map);
-        Map<String, String> param = new HashMap<>();
-        param.put("requestName", "集团会议通知");
-        param.put("workflowId", prop.getFwMeetingWorkflow());
-        param.put("mainData", JSONArray.fromObject(mapList).toString());
-        String resultInfo = JSONObject.toJSONString(param);
-        if ("true".equals(prop.getFlag())) {
-            System.out.println(resultInfo);
-        }
-        String back1 = GetFwTokenUtil.requestHttp(prop.getDocreate(), param, prop.getSendUserId());
-        if ("true".equals(prop.getFlag())) {
-            System.out.println("返回结果：" + back1);
-        }
-        if (back1.indexOf("requestid") != -1) {
-            JSONObject object = JSONObject.parseObject(back1);
-            Map data = (Map) object.get("data");
-            String requestid = data.get("requestid").toString();
-            String querySql = "select summary_id,fw_id from temp_fw_requrid_id where summary_id=" + id;
-            List<Map<String, Object>> listTemp = JDBCUtil.doQuery(querySql);
-
-            String insertsql = "insert into temp_fw_requrid_id(summary_id,fw_id) values(?,?)";
-            String updateSql = "update temp_fw_requrid_id set fw_id=? where summary_id=?";
-            Connection connection = null;
-            PreparedStatement ps = null;
-            try {
-                connection = JDBCAgent.getRawConnection();
-                if (listTemp.size() == 0) {
-                    ps = connection.prepareStatement(insertsql);
-                    ps.setString(1, id + "");
-                    ps.setString(2, requestid);
-                } else {
-                    ps = connection.prepareStatement(updateSql);
-                    ps.setString(1, requestid);
-                    ps.setString(2, id + "");
+                Map<String, String> param = new HashMap<>();
+                param.put("requestName", "集团会议通知");
+                param.put("workflowId", prop.getFwMeetingWorkflow());
+                param.put("mainData", JSONArray.fromObject(mapList).toString());
+                String resultInfo = JSONObject.toJSONString(param);
+                if ("true".equals(prop.getFlag())) {
+                    System.out.println(resultInfo);
                 }
-                ps.executeUpdate();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                closeUtil(connection, ps, null);
+                //sendUserId 这个值是根据股份公司的path 获取id ，然后去获取的
+                String back1 = GetFwTokenUtil.requestHttp(prop.getDocreate(), param, sendUserId);
+                if ("true".equals(prop.getFlag())) {
+                    System.out.println("返回结果：" + back1);
+                }
+                if (back1.indexOf("requestid") != -1) {
+                    JSONObject object = JSONObject.parseObject(back1);
+                    Map data = (Map) object.get("data");
+                    String requestid = data.get("requestid").toString();
+                    String querySql = "select summary_id,fw_id from temp_fw_requrid_id where summary_id=" + id;
+                    List<Map<String, Object>> listTemp = JDBCUtil.doQuery(querySql);
+
+                    String insertsql = "insert into temp_fw_requrid_id(summary_id,fw_id) values(?,?)";
+                    String updateSql = "update temp_fw_requrid_id set fw_id=? where summary_id=?";
+                    Connection connection = null;
+                    PreparedStatement ps = null;
+                    try {
+                        connection = JDBCAgent.getRawConnection();
+                        if (listTemp.size() == 0) {
+                            ps = connection.prepareStatement(insertsql);
+                            ps.setString(1, id + "");
+                            ps.setString(2, requestid);
+                        } else {
+                            ps = connection.prepareStatement(updateSql);
+                            ps.setString(1, requestid);
+                            ps.setString(2, id + "");
+                        }
+                        ps.executeUpdate();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        closeUtil(connection, ps, null);
+                    }
+                }
             }
         }
+
+
     }
 
     public void closeUtil(Connection connection, PreparedStatement ps, ResultSet rs) {
